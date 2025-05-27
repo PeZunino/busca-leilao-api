df2e8699ef20b2993350a01ea3aabdd92c4c8524
PeZunino <pedrozsantos@hotmail.com>
2025-05-27 17:47:55 -0300

    feat(committee): create entity

---
+++ src/domain/auctioneer/enterprise/entities/committee.ts
@@ -0,0 +1,19 @@
+import { Entity } from '@/shared/entity';
+
+interface Props{
+	name:string
+}
+
+export class Committee extends Entity<Props>{
+	public static create(props:Props){
+		return new Committee({name:props.name});
+	}
+  
+	get name():string{
+		return this.props.name;
+	}
+
+	public setName(newName: string): void {
+		this.props.name = newName;
+	}
+}
