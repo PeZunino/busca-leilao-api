cce83b7da20250e6a67310de77a3fc0a62a033f0
PeZunino <pedrozsantos@hotmail.com>
2025-05-27 16:58:45 -0300

    feat(test): create auctioneer test with factory

--- src/domain/auctioneer/application/use-case/create-auctioneer.spec.ts
+++ src/domain/auctioneer/application/use-case/create-auctioneer.spec.ts
@@ -19,7 +19,19 @@ describe('Create Auctioneer',()=>{
 
 		const auctioneer = makeAuctioneer();
 		
-		const result = await sut.execute(auctioneer);
+		const result = await sut.execute({
+			cep: auctioneer.address.cep,
+			city: auctioneer.address.city,
+			email: auctioneer.email.address,
+			name: auctioneer.name,
+			neighborhood: auctioneer.address.neighborhood,
+			number: auctioneer.address.number,
+			phoneNumber: auctioneer.phoneNumber.rawNumber,
+			registrationCode: auctioneer.registrationCode,
+			state: auctioneer.address.state,
+			street: auctioneer.address.street,
+			websites: auctioneer.websites.map(website=>website.url.toString())
+		});
 	
 		expect(result.isSuccessful())
 			.toBe(true);
