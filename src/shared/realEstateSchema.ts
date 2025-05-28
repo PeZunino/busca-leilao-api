3e380024d239746d6772e6859fb3ef410a8b3b9d
PeZunino <pedrozsantos@hotmail.com>
2025-05-28 20:08:21 -0300

    refactor(zod): schema validation

--- src/shared/realEstateSchema.ts
+++ src/shared/realEstateSchema.ts
@@ -1,30 +1,10 @@
 
 import z from 'zod';
-import { RealEstateInput, RealEstateProps } from '@/domain/entities/realEstate/realEstate';
-import { Address } from '@/domain/valueObjects/address';
-import { Area } from '@/domain/valueObjects/area';
-import { Currency, Real } from '@/domain/valueObjects/real';
 
 export function applyRealEstateTransformsAndRefines<T extends z.ZodObject<any>>(
 	baseSchema: T
 ) { 
 	return baseSchema
-		.transform((data) => {
-			
-			const inputData = data as RealEstateInput;
-
-			return {
-				...inputData,
-				totalArea: Area.create(inputData.totalArea.value, inputData.totalArea.unit),
-				builtArea: Area.create(inputData.builtArea.value, inputData.builtArea.unit),
-				privateArea: Area.create(inputData.privateArea.value, inputData.privateArea.unit),
-				fieldArea: Area.create(inputData.fieldArea.value, inputData.fieldArea.unit),
-				address: Address.create(inputData.address),
-				debits: Real.create(inputData.debits, Currency.BRL),
-				startingBid: Real.create(inputData.startingBid, Currency.BRL),
-				initialValue: Real.create(inputData.initialValue, Currency.BRL),
-			} as RealEstateProps; 
-		})
 		.refine((data) => {
 			
 			return data.builtArea.toSquareMeters() <= data.totalArea.toSquareMeters();
--- src/shared/vehicleSchema.ts
+++ src/shared/vehicleSchema.ts
@@ -1,24 +1,12 @@
 
 import z from 'zod';
-import { VehicleInput, VehicleProps } from '@/domain/auctioneer/enterprise/entities/vehicle/vehicle';
-import { Currency,Real } from '@/domain/auctioneer/enterprise/valueObjects/real';
 
 export function applyVehicleTransformsAndRefines<T extends z.ZodObject<any>>(
 	baseSchema: T
 ) { 
 	return baseSchema
-		.transform((data) => {
-			const inputData = data as VehicleInput; 
 
-			return {
-				...inputData,
-				startingBid: Real.create(inputData.startingBid, Currency.BRL),
-				initialValue: Real.create(inputData.initialValue, Currency.BRL),
-			} as VehicleProps; 
-		})
 		.refine((data) => {
-			
-			
 			return data.yearModel >= data.year;
 		}, {
 			message: 'Model year cannot be earlier than manufacture year',
