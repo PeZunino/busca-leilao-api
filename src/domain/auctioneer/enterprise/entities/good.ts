92f0f27c9ce4fbbd4c5fdffd32fa0ad83a20a398
PeZunino <pedrozsantos@hotmail.com>
2025-05-29 12:13:29 -0300

    feat(good): factory

--- src/domain/auctioneer/enterprise/entities/good.ts
+++ src/domain/auctioneer/enterprise/entities/good.ts
@@ -1,29 +1,44 @@
+import { BuiltProperty, BuiltPropertyDTO } from './realEstate/builtProperty';
+import { UnbuiltProperty, UnbuiltPropertyDTO } from './realEstate/unbuiltProperty';
+import { Car, CarDTO } from './vehicle/car';
+import { Motorcycle, MotorcycleDTO } from './vehicle/motorcycle';
 
-import { Entity } from '@/core/shared/entity';
-import { Real } from '../valueObjects/real';
-
-export interface GoodProps {
-	startingBid: Real; 
-	initialValue: Real; 
+export enum GoodType {
+	CAR = 'car',
+	MOTORCYCLE = 'motorcycle',
+	BUILT_PROPERTY = 'built',
+	UNBUILT_PROPERTY = 'unbuilt'
 }
 
-export interface GoodInput {
-	origin:string
-	startingBid: number; 
-	description: string;
-	observation: string;
-	initialValue: number; 
-}
+export type GoodObject = Car | Motorcycle | BuiltProperty | UnbuiltProperty;
 
-export abstract class Good extends Entity<GoodProps> {
-	
-	get startingBid(): Real {
-		return this.props.startingBid;
-	}
+export type GoodDTO = 
+  | { type: GoodType.CAR;
+  	data: CarDTO }
+  | { type: GoodType.MOTORCYCLE;
+  	data: MotorcycleDTO }
+  | { type: GoodType.BUILT_PROPERTY;
+  	data: BuiltPropertyDTO }
+  | { type: GoodType.UNBUILT_PROPERTY;
+  	data: UnbuiltPropertyDTO };
 
-	get initialValue(): Real {
-		return this.props.initialValue;
+export class GoodFactory {
+	static create(input: GoodDTO): GoodObject {
+		switch(input.type) {
+			case GoodType.CAR:
+				return Car.create(input.data);
+      
+			case GoodType.MOTORCYCLE:
+				return Motorcycle.create(input.data);
+      
+			case GoodType.BUILT_PROPERTY:
+				return BuiltProperty.create(input.data);
+      
+			case GoodType.UNBUILT_PROPERTY:
+				return UnbuiltProperty.create(input.data);
+      
+			default:
+				throw new Error(`Invalid good type: ${(input as any).type}`);
+		}
 	}
-	
-	public abstract getCategory(): string;
 }
